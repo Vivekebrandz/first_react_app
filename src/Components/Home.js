100e9a31722bb0f6bde90273604e29a5ddd7f1e7
Vivek Mandal <vivek.m@ebrandz.com>
2024-06-09 02:40:48 +0530

    changes

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import BannerCarousel from './BannerCarousel';
-import plane from '../images/plane.png';
+import plane from '../images/plane.gif';
 
 const Button = (params) => {
   const [buttonName, setButtonName] = useState(params.name);
